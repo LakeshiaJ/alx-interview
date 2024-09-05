de61cb334c8a1a2047ea4cca0521f520c492f081
LakeshiaJ <joseph.mayaka@yahoo.com>
2024-09-05 17:33:33 +0300

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,7 @@
+{
+  "name": "0x06-starwars_api",
+  "version": "1.0.0",
+  "description": "<p><h1>0x06. Star Wars API :fire:</h1></p>",
+  "main": "0-starwars_characters.js",
+  "type": "commonjs",
+  "scripts": {
