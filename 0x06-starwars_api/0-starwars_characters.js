41c5a4830ddfe977c155f681a700959c57d9e3c4
LakeshiaJ <joseph.mayaka@yahoo.com>
2024-09-05 17:35:54 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,6 @@
-{
-  "name": "0x06-starwars_api",
-  "version": "1.0.0",
-  "description": "<p><h1>0x06. Star Wars API :fire:</h1></p>",
-  "main": "0-starwars_characters.js",
-  "type": "commonjs",
-  "scripts": {
+  });
+};
+get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`)
+  .then(response => Promise.all(response.characters.map(url => get(url))))
+  .then(responses => responses.map(response => response.name))
+  .then(names => names.forEach(name => console.log(name)))
