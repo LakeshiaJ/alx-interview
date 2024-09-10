b61102da4d8ae45c139185d15306c0b039057392
LakeshiaJ <joseph.mayaka@yahoo.com>
2024-09-10 13:20:56 +0300

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,28 @@
-  });
-};
-get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`)
-  .then(response => Promise.all(response.characters.map(url => get(url))))
-  .then(responses => responses.map(response => response.name))
-  .then(names => names.forEach(name => console.log(name)))
+#!/usr/bin/node
+
+const request = require('request');
+
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
+
+// Makes an API request to get film information
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
+
+  // Parse the response body to get the list of character URLs
+  const charURLList = JSON.parse(body).characters;
+
+  // Iterare through the character URLs and fect character information
+  // Make a request to each character URL
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
+
+        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
