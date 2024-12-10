044a41e038c6df89c793413e459df9cf12fee72e
LakeshiaJ <joseph.mayaka@yahoo.com>
2024-12-10 19:27:53 +0300

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Prime game module.
 """
+
+
 def isWinner(x, nums):
-    """Determines the winner of a prime game session with `x` rounds.
-    """
+    """Determines the winner of a prime game session with `x` rounds."""
     if x < 1 or not nums:
         return None
     marias_wins, bens_wins = 0, 0
@@ -18,9 +19,9 @@ def isWinner(x, nums):
             primes[j - 1] = False
     # filter the number of primes less than n in nums for each round
     for _, n in zip(range(x), nums):
-        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        primes_count = len(list(filter(lambda x: x, primes[0:n])))
         bens_wins += primes_count % 2 == 0
         marias_wins += primes_count % 2 == 1
     if marias_wins == bens_wins:
         return None
-    return 'Maria' if marias_wins > bens_wins else 'Ben'"
+    return "Maria" if marias_wins > bens_wins else "Ben"
